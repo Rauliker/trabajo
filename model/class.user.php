fef52f85cb3393440a5581cd513cb20dbe934a54
Raul <rauliker04@gmail.com.com>
2024-05-20 15:44:59 +0200

    v2

--- model/class.user.php
+++ model/class.user.php
@@ -1,17 +1,20 @@
 <?php
 require_once('class.database.php');
 
-class user {
+class user
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new Database();
     }
 
-    public function añadir() {
+    public function añadir()
+    {
         // Comprobamos que both variables have been sent
         if (isset($_POST['user_id']) && isset($_POST['user_name']) && isset($_POST['user_lastname']) && isset($_POST['passw']) && isset($_POST['email']) && isset($_POST['tel']) && isset($_POST['passw_val'])) {
-            
+
             $id = $_POST['user_id'];
             $name = $_POST['user_name'];
             $lastname = $_POST['user_lastname'];
@@ -19,8 +22,8 @@ class user {
             $email = $_POST['email'];
             $tel = $_POST['tel'];
             $passw_val = $_POST['passw_val'];
-            
-            if ($passw!=$passw_val) {
+
+            if ($passw != $passw_val) {
                 echo "Las contraseñas no coinciden.";
             } else {
                 // Consulta para comprobar si el usuario ya existe
@@ -30,21 +33,21 @@ class user {
                 $stmt->execute();
                 $result = $stmt->get_result();
                 $users = $result->fetch_all(MYSQLI_ASSOC);
-        
+
                 // Si no existe el usuario, lo insertamos
-                
+
                 if (empty($users)) {
                     foreach ($users as $key => $value) {
                         echo $value;
                     }
-                    
+
                     $visible = 1;
                     $sql = "INSERT INTO user(user_id, name, lastname, password, email, tel, visible) VALUES ('$id', '$name', '$lastname', '$passw', '$email', '$tel', $visible);";
                     $stmt = $this->db->connection->prepare($sql);
-                    
+
                     //$stmt->bind_param("ssssssi", $id, $name, $lastname, $passw, $email, $tel, $visible);
                     $result = $stmt->execute();
-        
+
                     if ($result) {
                         echo "Usuario creado correctamente";
                         // Iniciamos la sesión y guardamos el valor
@@ -57,42 +60,43 @@ class user {
                     echo "Usuario ya existente";
                 }
             }
-            
         } else {
             echo "Faltan parámetros";
         }
     }
 
-    public function ver_user() {
+    public function ver_user()
+    {
         $sql = "SELECT * FROM user;";
 
         $result = $this->db->connection->query($sql);
         return $result->fetch_all(MYSQLI_ASSOC);
-        
     }
 
-    public function acceder() {
+    public function acceder()
+    {
         if (isset($_POST['user_id']) && isset($_POST['passw']) && isset($_POST['email'])) {
             $id = $_POST['user_id'];
             $passw = $_POST['passw'];
             $email = $_POST['email'];
-    
+
             // Consulta para comprobar si el usuario ya existe
-            $sql1 = "SELECT * FROM user WHERE user_id = '". $id ."' AND email = '". $email ."' AND  visible=1;";
+            $sql1 = "SELECT * FROM user WHERE user_id = '" . $id . "' AND email = '" . $email . "' AND  visible=1;";
             $stmt = $this->db->connection->prepare($sql1);
             //$stmt->bind_param("ss", $id, $email);
             $stmt->execute();
             $result = $stmt->get_result();
             $users = $result->fetch_all(MYSQLI_ASSOC);
-    
+
             // Si el usuario existe, lo iniciamos y guardamos el valor
             if (!empty($users)) {
                 // Verificamos si la contraseña es correcta
                 if ($users[0]['password'] == $passw) {
                     // Iniciamos la sesión y guardamos el valor
-                    $_SESSION["usuario"] = true;
+
                     foreach ($users as $key => $value) {
-                        echo "Bienvenido " . $value['user_id'];
+                        $_SESSION["usuario"] = $value['user_id'];
+                        echo "Bienvenido " . $_SESSION["usuario"];
                     }
                 } else {
                     echo "Contraseña incorrecta";
@@ -105,4 +109,3 @@ class user {
         }
     }
 }
-?>
--- model/class.vender.php
+++ model/class.vender.php
@@ -1,15 +1,19 @@
 <?php
+session_start();
 require_once('class.database.php');
 
-class vender {
+class vender
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = new Database();
     }
 
-    public function añadir() {
-        
+    public function añadir()
+    {
+
         $name = $_POST['name'];
         $descrip = $_POST['descrip'];
         $tipo = $_POST['tipo'];
@@ -21,30 +25,30 @@ class vender {
         $construccion = $_POST['construccion'];
         $estado = $_POST['estado'];
         $superficie = $_POST['superficie'];
-        $pro=$this->bus("provincias",$_POST['provincias']);
-        $mun=$this->bus("municipios",$_POST['municipios']);
+        $pro = $this->bus("provincias", $_POST['provincias']);
+        $mun = $this->bus("municipios", $_POST['municipios']);
+        $user = $_SESSION['usuario'];
+        $enlace = $name . "-" . $pro . "-" . $mun;
 
-        $enlace=$name."-".$pro."-".$mun;
-        
-        $sql = "SELECT * FROM vivienda WHERE enlace ='".$enlace."'";
+        $sql = "SELECT * FROM vivienda WHERE enlace ='" . $enlace . "'";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->execute();
         $result = $stmt->get_result();
         $ver = $result->fetch_all(MYSQLI_ASSOC);
 
         foreach ($ver as $key => $value) {
-            if ($enlace==$value['enlace']) {
-                $enlace=$name."-".$pro."-".$mun."-".$value['id'];
+            if ($enlace == $value['enlace']) {
+                $enlace = $name . "-" . $pro . "-" . $mun . "-" . $value['id'];
             }
-        }        
+        }
 
         $visible = 1;
-        $sql = "INSERT INTO vivienda(id, nombre, descrippcion, tipo, provincia, municiopio, direccion, precio, habitaciones, superficie, construccion, estado, visible, enlace) VALUES (NULL, '$name', '$descrip', '$tipo', '$provincia', '$municipios', '$direccion', $prec, $nhab, $superficie, '$construccion', '$estado', $visible, '$enlace');";
+        $sql = "INSERT INTO vivienda(id, nombre, descrippcion, tipo, provincia, municiopio, direccion, precio, habitaciones, superficie, construccion, estado, visible, enlace ,user) VALUES (NULL, '$name', '$descrip', '$tipo', '$provincia', '$municipios', '$direccion', $prec, $nhab, $superficie, '$construccion', '$estado', $visible, '$enlace' ,'$user');";
         $stmt = $this->db->connection->prepare($sql);
-        
+
         //$stmt->bind_param("ssssssi", $id, $name, $lastname, $passw, $email, $tel, $visible);
         $result = $stmt->execute();
-        $id=$this->db->connection->insert_id;
+        $id = $this->db->connection->insert_id;
 
         if ($result) {
             echo "Vivienda añadida correctamente";
@@ -56,18 +60,18 @@ class vender {
         // Verifica si se envió el formulario
         if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"])) {
             if (isset($_FILES["img"])) {
-                
+
                 for ($i = 0; $i < count($_FILES["img"]["name"]); $i++) {
                     $file = $_FILES["img"];
                     $file_name = $file["name"][$i];
-                    
+
                     $file_type = $file["type"][$i];
-                    if ("image/png"==$file_type) {
-                        $a=$i.".png";
+                    if ("image/png" == $file_type) {
+                        $a = $i . ".png";
                     } else {
-                        $a=$i.".jpg";
+                        $a = $i . ".jpg";
                     }
-                    
+
                     $file_size = $file["size"][$i];
                     $file_tmp_name = $file["tmp_name"][$i];
                     $file_error = $file["error"][$i];
@@ -75,14 +79,14 @@ class vender {
                     if ($file_error === UPLOAD_ERR_OK) {
                         $target_dir = "images/" . $id . "/";
 
-                    if (!file_exists($target_dir)) {
-                        mkdir($target_dir, 0777, true);
-                    }
-                        
+                        if (!file_exists($target_dir)) {
+                            mkdir($target_dir, 0777, true);
+                        }
+
                         move_uploaded_file($file_tmp_name, $target_dir . $a);
 
-                        move_uploaded_file($file_tmp_name, "images/".$id."/" . $a);
-                        $file_direc= $id."/" . $a;
+                        move_uploaded_file($file_tmp_name, "images/" . $id . "/" . $a);
+                        $file_direc = $id . "/" . $a;
 
                         $stmt = $this->db->connection->prepare("INSERT INTO imagen(imagen, id_vivienda,enlace) VALUES (?, ?, ?)");
                         $stmt->bind_param("sds", $file_direc, $id, $enlace);
@@ -95,48 +99,49 @@ class vender {
                         echo "Error al subir el archivo. Código de error: " . $file_error;
                     }
                 }
-                
             } else {
                 echo "No se recibió ningún archivo.";
             }
         }
     }
 
-    public function table($name) {
+    public function table($name)
+    {
         $sql = "SELECT * FROM $name";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->execute();
         $result = $stmt->get_result();
         $propertyTypes = $result->fetch_all(MYSQLI_ASSOC);
-            echo "<label>$name:</label>
+        echo "<label>$name:</label>
             <br>
             <select type='input' id='$name' name='$name' placeholder='Property Type' required>
             ";
-        if ($name=="municipios") {
+        if ($name == "municipios") {
             foreach ($propertyTypes as $propertyType) {
                 if (is_array($propertyType) && array_key_exists("id", $propertyType)) {
-                    echo "<option value='". $propertyType['id']. "' name='". $propertyType['id_provincia']. "'>".$propertyType['nombre']. "</option>";
+                    echo "<option value='" . $propertyType['id'] . "' name='" . $propertyType['id_provincia'] . "'>" . $propertyType['nombre'] . "</option>";
                 }
             }
-        }else if($name=="tipo") {
+        } else if ($name == "tipo") {
             foreach ($propertyTypes as $propertyType) {
                 if (is_array($propertyType) && array_key_exists("id", $propertyType)) {
-                    echo "<option value='". $propertyType['nombre']. "'>".$propertyType['nombre']. "</option>";
+                    echo "<option value='" . $propertyType['nombre'] . "'>" . $propertyType['nombre'] . "</option>";
                 }
             }
-        }else {
+        } else {
             foreach ($propertyTypes as $propertyType) {
                 if (is_array($propertyType) && array_key_exists("id", $propertyType)) {
-                    echo "<option value='". $propertyType['id']. "'>".$propertyType['nombre']. "</option>";
+                    echo "<option value='" . $propertyType['id'] . "'>" . $propertyType['nombre'] . "</option>";
                 }
             }
         }
-        
-        echo"</select>
+
+        echo "</select>
         <br>";
     }
 
-    public function buscar(){
+    public function buscar()
+    {
         $sql = "SELECT * FROM provincias";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->execute();
@@ -147,17 +152,15 @@ class vender {
         file_put_contents('assets/js/provincias.json', $json);
     }
 
-    public function bus($bbb,$aaa){
+    public function bus($bbb, $aaa)
+    {
         $sql = "SELECT nombre FROM $bbb WHERE id = $aaa";
         $stmt = $this->db->connection->prepare($sql);
         $stmt->execute();
         $result = $stmt->get_result();
         $fila = $result->fetch_row();
         foreach ($fila as $key => $value) {
-            return($value);
+            return ($value);
         }
-        
     }
-    
 }
-?>
